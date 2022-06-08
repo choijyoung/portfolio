6f840924241d24e8318279f14dc4518a8942345e
choijyoung <choi.jae.y@gmail.com>
2022-06-07 22:34:48 -0400

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,29 @@
+import "./navbar.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="header">
+        <Link to="/">
+            <h1>Hello</h1>
+        </Link>
+        <ul className="navmenu">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/project">Projects</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar

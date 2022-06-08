6d0b953e77c1f276955461a0057b383396f7a743
choijyoung <choi.jae.y@gmail.com>
2022-06-07 22:02:16 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./index.css"
+import Home from "./routes/home"
+import About from "./routes/about"
+import Project from "./routes/project"
+import Contact from "./routes/contact"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-  <div>
-    <h1>This is my Portfolio</h1>
-  </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 

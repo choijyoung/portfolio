34bb32ff25cff047f47dee03fbf3db89fa875663
choijyoung <choi.jae.y@gmail.com>
2022-06-18 13:00:57 -0400

    hero image project page

---
+++ src/components/Heroimg2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroImg2.css'
+
+const Heroimg2 = () => {
+  return (
+    <div className='heroimg'>
+        <div className='heading'>
+            <h1>This is a heading</h1>
+            <p>This is a text</p>
+        </div>
+    </div>
+  )
+}
+
+export default Heroimg2
--- src/routes/project.js
+++ src/routes/project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Heroimg2 from '../components/Heroimg2'
 
 const Project = () => {
   return (
     <div>
       <Navbar />
+      <Heroimg2 />
       <Footer />
     </div>
   )

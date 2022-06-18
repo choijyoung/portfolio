be5003df1e0b38965387b1154c337b1755e9afc8
choijyoung <choi.jae.y@gmail.com>
2022-06-18 15:36:54 -0400

    dynamic component

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
+import Heroimg2 from '../components/Heroimg2'
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <Heroimg2 heading="Contact" text="You can contact me here" />
       <Footer />
     </div>
   )
